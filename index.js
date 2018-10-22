730e82763cf9e1ead2a1a81deb474759f7f89559
David Sollesse <davidsollesse@gmail.com>
2018-10-22 13:54:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ console.log(string.toLowerCase())
 function sayHiToGrandma(string) {
 if(string.toLowerCase() === string)
   return('I can\'t hear you!')
-} else {(string.toUpperCase() === string)
-  return "YES INDEED!"}
+}
+ else {(string.toUpperCase() === string)
+  return "YES INDEED!"
+}
  else {(string = 'I love you, Grandma.')
   return "I love you too"
 }
