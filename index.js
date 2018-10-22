caab5df3786f1c731631405be17a04b9f5064bff
David Sollesse <davidsollesse@gmail.com>
2018-10-22 13:55:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ function logWhisper(string) {
 console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-if(string.toLowerCase() === string)}
+function sayHiToGrandma(string)
+if{(string.toLowerCase() === string)}
   return('I can\'t hear you!')
  else {(string.toUpperCase() === string)}
   return "YES INDEED!"
